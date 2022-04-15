d3542efc8512f5c6a9670aab93f3ff65f3b71474
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-15 12:43:40 -0400

    coffeess

---
+++ models/coffee.models.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose');
+
+const coffeeSchema =  new Schema({
+    country: {
+        type: String,
+        enum: ["Colombian", "Ethiopian", "Brazilian", "Nicaraguan", "Costa Rican"],
+        required: true
+    },
+    roast: {
+        type: String,
+        enum: ["dark", "light"],
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+})
+
+const Coffee =  model("coffee", coffeeSchema)
+
+module.exports = Coffee

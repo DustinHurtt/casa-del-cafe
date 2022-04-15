78adb3f6d9955dfa9a9ef8001a940b1b6f4a0910
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-04-15 16:26:39 -0400

    updated seed

--- bin/seed.js
+++ bin/seed.js
@@ -4,44 +4,44 @@ const Food = require('../models/Food.models')
 
 let newFoods = [
     {
-    name: "Jhon" ,
-    category: "Smith",
-    description: "Miami",
-    ingredients: 33136,
-    calories: "Pizza",
-    price: "Morning Person"
+    name: "Panini" ,
+    category: "Lunch",
+    description: "Grilled and pressed sandwich",
+    ingredients: "bread, meat, cheese, vegetables",
+    calories: 700,
+    price: 8.99
   },
   {
-    name: "Jhon" ,
-    category: "Smith",
-    description: "Miami",
-    ingredients: 33136,
-    calories: "Pizza",
-    price: "Morning Person"
+    name: "Pastry" ,
+    category: "Breakfast",
+    description: "Buttery, flaky, and sweet",
+    ingredients: "flour, butter, sugar, fruit",
+    calories: 400,
+    price: 5.99
   },
   {
-    name: "Jhon" ,
-    category: "Smith",
-    description: "Miami",
-    ingredients: 33136,
-    calories: "Pizza",
-    price: "Morning Person"
+    name: "Cobb Salad" ,
+    category: "Lunch",
+    description: "Hearty, fresh, and flavorful salad",
+    ingredients: "lettuce, bacon, tomato, egg, bleu cheese",
+    calories: 600,
+    price: 7.99
   },
   {
-    name: "Jhon" ,
-    category: "Smith",
-    description: "Miami",
-    ingredients: 33136,
-    calories: "Pizza",
-    price: "Morning Person"
+    name: "Spaghetti Aglio e Olio" ,
+    category: "Lunch",
+    description: "Light and tasty pasta",
+    ingredients: "pasta, olive oil, garlic, red pepper flakes, parmesan, ",
+    calories: 650,
+    price: 7.50
   },
   {
-    name: "Jhon" ,
-    category: "Smith",
-    description: "Miami",
-    ingredients: 33136,
-    calories: "Pizza",
-    price: "Morning Person"
+    name: "Fruit Tart" ,
+    category: "Dessert",
+    description: "A light and sweet dessert.",
+    ingredients: "fruit, berries, flour, eggs, sugar",
+    calories: 400,
+    price: 5.99
   }
   ]
 
@@ -49,35 +49,35 @@ const Coffee = require('../models/coffee.models')
 
 let newCoffees = [
     {
-    country: "Jhon" ,
-    roast: "Smith",
-    description: "Miami",
-    price: 33136,
+    country: "Colombian" ,
+    roast: "Dark",
+    description: "From the mountains of Colombia",
+    price: 5.99,
 
   },
   {
-    country: "Jhon" ,
-    roast: "Smith",
-    description: "Miami",
-    price: 33136,
+    country: "Brazillian" ,
+    roast: "Dark",
+    description: "Therefore, medium-dark roast is preferred, as a dark roast can project an ashy, bitter flavor. Premium Brazil coffee is low in acidity, exhibits a nutty sweet flavor, and is often bittersweet with a chocolaty roast taste.",
+    price: 6.50,
   },
   {
-    country: "Jhon" ,
-    roast: "Smith",
-    description: "Miami",
-    price: 33136,
+    country: "Nicaraguan" ,
+    roast: "Dark",
+    description: "The aroma has sweet caramel, citrus, and chocolate elements, while the coffee itself can have a very pleasant, balanced, and bittersweet flavor.",
+    price: 7.50,
   },
   {
-    country: "Jhon" ,
-    roast: "Smith",
-    description: "Miami",
-    price: 33136,
+    country: "Ethiopian" ,
+    roast: "Light",
+    description: "Ethiopian beans as a whole are known for their winey quality and bright mouthfeels.",
+    price: 6.99,
   },
   {
-    country: "Jhon" ,
-    roast: "Smith",
-    description: "Miami",
-    price: 33136,
+    country: "Costa Rican" ,
+    roast: "Light",
+    description: "In particular the best high-grown coffees of Costa Rica are distinguished, and notable for their good body and fruity and bright acidity with a clean and crisp taste.",
+    price: 4.50,
   }
   ]
 

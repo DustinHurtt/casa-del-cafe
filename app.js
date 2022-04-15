6cea40b741c4b1eb88e4412216007d5aaa9ac477
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-15 11:55:22 -0400

    views

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require ('mongoose') ;
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,8 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
